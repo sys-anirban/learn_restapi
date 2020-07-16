ad64e8efe3e64626b703a8569b73396f19b26461
Anirban <anirbanghatak92@gmail.com>
2020-07-16 18:53:19 +0530

    Edit Post

--- middleware.js
+++ middleware.js
@@ -2,11 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 const path = require("path");
+const multer = require("multer");
+const cors = require("cors");
+const moment = require("moment");
+
 const app = express();
+app.use(cors());
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, moment() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
